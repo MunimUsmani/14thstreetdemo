0fd1441d6e4938f4f2f058fab57f26fbdec8817a
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2024-10-13 04:04:58 -0700

    Location  page done

--- src/app/Location/page.tsx
+++ src/app/Location/page.tsx
@@ -1,11 +1,15 @@
 import NavBar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
+import SearchBar from "@/app/components/SearchBar";
+import Location from "@/app/components/Location";
 
 const Home: React.FC = () => {
   return (
     <>
       <div>
         <NavBar />
+        <SearchBar />
+        <Location />
         <Footer />
       </div>
     </>
